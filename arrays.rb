135601a91fe40fa8f0544e7a23de31812728f46f
Andra Helton <andrahelton@gmail.com>
2022-11-02 18:17:34 -0600

    Add index position notes

--- arrays.rb
+++ arrays.rb
@@ -16,4 +16,6 @@ p heights_feet
 # unshift method adds 1 or more elements to the beginning of array and returns the new array
 p is_here.unshift (false)
 
-
+# Index Positions:
+# The index position refers to the location of an element within the array.
+# The order begins with 0 - ex 0, 1, 2, etc. So the first element in an array is 0.
