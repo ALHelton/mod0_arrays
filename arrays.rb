72d094e703a30b0206e74e9939c6a910e98ebbe2
Andra Helton <andrahelton@gmail.com>
2022-11-02 18:14:15 -0600

    Add array methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,18 @@ names = ["Amy", "Fred", "Bob", "Jan"]
 ages = [28, 65, 35, 42]
 heights_feet = [5.7, 6.1, 5.3, 6.7]
 is_here = [true, false, false, false]
+
+# pop method takes out the last element and returns it
+p names.pop
+
+# push method adds 1 or more elements to the end and returns the new array
+p ages.push (65)
+
+# shift method removes first element and returns it
+p heights_feet.shift(2)
+p heights_feet
+
+# unshift method adds 1 or more elements to the beginning of array and returns the new array
+p is_here.unshift (false)
+
+
